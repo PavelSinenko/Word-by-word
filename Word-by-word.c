3c4158b6e47f3226d950137d5f052c68d05aaba2
Pavel Sinenka <sinenko.pavel72@gmail.com>
2026-03-05 14:02:44 +0400

    Добавил рандом для букв и позиций

--- Word-by-word.c
+++ Word-by-word.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main () {
-    char letters[] = {"a"};
-    int position = 2;
-    printf("Придумайте слово, в котором будет буква: %s\n", letters);
-    printf("Эта буква должна быть в слове на позиции номер: %i\n", position);
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int lettersCount = sizeof(letters) / sizeof(letters[0]);
+    int positions[] = {1, 2, 3};
+    int positionsCount = sizeof(positions) / sizeof(positions[0]);
+
+    srand(time(NULL));
+
+    int randomLetter = rand() % lettersCount;
+    int randomPosition = rand() % positionsCount;
+
+    printf("Придумайте слово, в котором будет буква: %c\n", letters[randomLetter]);
+    printf("Эта буква должна быть в слове на позиции номер: %i\n", positions[randomPosition]);
 
     char word[20];
 
@@ -13,7 +23,7 @@ int main () {
     fgets(word, sizeof(word), stdin);   
     word[strcspn(word,"\n")] = 0;       
 
-    if (word[position - 1] == letters[0]) {
+    if (word[randomPosition] == letters[randomLetter]) {
         printf("Красава, хорошо придумал!");
     } else {
         printf("Не, слово не подходит");
